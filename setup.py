9e7525d751d0cea262e7176b979bf24aab4106db
Stanley F <stanley.foerster@gmail.com>
2025-09-17 14:23:56 +0200

    allow tensorflow 2.19

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ extras_require = {
     ],
     "tensorflow": [
         "tensorflow_probability>=0.8.0, <0.26.0",
-        "tensorflow>=2.16.0, <2.19.0",
-        "tf-keras>=2.16.0, <2.19.0",
+        "tensorflow>=2.16.0, <2.20.0",
+        "tf-keras>=2.16.0, <2.20.0",
     ],
     "keops": [
         "pykeops>=2.0.0, <2.3.0",
@@ -28,8 +28,8 @@ extras_require = {
     "all": [
         "prophet>=1.1.0, <2.0.0",
         "tensorflow_probability>=0.8.0, <0.26.0",
-        "tensorflow>=2.16.0, <2.19.0",
-        "tf-keras>=2.16.0, <2.19.0",
+        "tensorflow>=2.16.0, <2.20.0",
+        "tf-keras>=2.16.0, <2.20.0",
         "pykeops>=2.0.0, <2.3.0",
         "torch>=1.7.0, <2.6.0"
     ],
